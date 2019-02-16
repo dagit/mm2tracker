f5eabd20bfc7a4da69693e48f66b249c495809e6
Jason Dagit <dagitj@gmail.com>
2019-02-16 13:32:31 -0800

    native: layout all the buttons

--- native/src/main.rs
+++ native/src/main.rs
@@ -5,6 +5,11 @@ use winapi::shared::windef::HWND;
 
 static APP_NAME : &str = "mm2tracker";
 
+const ROBO_PORTRAIT_WIDTH  : i32 = 54;
+const ROBO_PORTRAIT_HEIGHT : i32 = 60;
+const ITEM_PORTRAIT_WIDTH  : i32 = 37;
+const ITEM_PORTRAIT_HEIGHT : i32 = 20;
+
 fn as_wstr(s: &str) -> Vec<u16> {
     use std::ffi::OsStr;
     use std::iter::once;
@@ -19,17 +24,31 @@ struct Window {
 fn create_window(name: &str, title: &str) -> Result<Window, Error> {
     use std::ptr::null_mut;
     use winapi::um::libloaderapi::GetModuleHandleW;
+    use winapi::shared::windef::{
+        RECT,
+    };
     use winapi::um::winuser::{
-        DefWindowProcW,
-        RegisterClassW,
+        AdjustWindowRectEx,
         CreateWindowExW,
+        DefWindowProcW,
+        GetWindowLongW,
+        LoadCursorW,
+        MoveWindow,
+        RegisterClassW,
         WNDCLASSW,
+        BS_ICON,
+        BS_AUTOCHECKBOX,
+        BS_PUSHLIKE,
         CS_OWNDC,
         CS_HREDRAW,
         CS_VREDRAW,
         CW_USEDEFAULT,
+        GWL_STYLE,
+        GWL_EXSTYLE,
+        IDC_ARROW,
         WS_OVERLAPPEDWINDOW,
         WS_VISIBLE,
+        WS_CHILD,
     };
 
     let name = as_wstr(name);
@@ -37,7 +56,7 @@ fn create_window(name: &str, title: &str) -> Result<Window, Error> {
 
     let hinstance = unsafe { GetModuleHandleW( null_mut() ) };
 
-    let wnd_class = WNDCLASSW {
+    let wnd_class = unsafe { WNDCLASSW {
         style: CS_OWNDC | CS_HREDRAW | CS_VREDRAW,
         lpfnWndProc : Some( DefWindowProcW ),
         hInstance: hinstance,
@@ -45,10 +64,10 @@ fn create_window(name: &str, title: &str) -> Result<Window, Error> {
         cbClsExtra: 0,
         cbWndExtra: 0,
         hIcon: null_mut(),
-        hCursor: null_mut(),
+        hCursor: LoadCursorW(null_mut(), IDC_ARROW),
         hbrBackground: null_mut(),
         lpszMenuName: null_mut(),
-    };
+    }};
     unsafe { RegisterClassW( &wnd_class ) };
 
     let handle = unsafe { CreateWindowExW(
@@ -58,19 +77,74 @@ fn create_window(name: &str, title: &str) -> Result<Window, Error> {
         WS_OVERLAPPEDWINDOW | WS_VISIBLE,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
+        0,
+        0,
         null_mut(),
         null_mut(),
         hinstance,
         null_mut(),
     ) };
 
-    if handle.is_null() {
-        Err( Error::last_os_error() )
-    } else {
-        Ok( Window { handle } )
+    if handle.is_null() { return Err( Error::last_os_error() ) }
+
+    //calculate the window size based on a desired client rect size
+    let mut window_rect = RECT {
+        left: 0,
+        top: 0,
+        right: ROBO_PORTRAIT_WIDTH * 8 + ITEM_PORTRAIT_WIDTH,
+        bottom: std::cmp::max(ROBO_PORTRAIT_HEIGHT, ITEM_PORTRAIT_HEIGHT * 3),
+    };
+    let ok = unsafe { AdjustWindowRectEx(
+        &mut window_rect,
+        GetWindowLongW(handle, GWL_STYLE) as u32,
+        false as i32,
+        GetWindowLongW(handle, GWL_EXSTYLE) as u32 )
+    };
+    if ok == 0 { return Err( Error::last_os_error() ) }
+
+    //Now resize the window
+    let ok = unsafe { MoveWindow(
+        handle,
+        0, 0,
+        window_rect.right - window_rect.left,
+        window_rect.bottom - window_rect.top,
+        true as i32)
+    };
+    if ok == 0 { return Err( Error::last_os_error() ) }
+
+    let button_style = BS_ICON | WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX | BS_PUSHLIKE;
+
+    // Place the buttons for each robo master
+    for i in 0..8 {
+        let _ : HWND = unsafe { CreateWindowExW(
+            0,
+            as_wstr("BUTTON").as_ptr(),
+            as_wstr("").as_ptr(),
+            button_style,
+            i*ROBO_PORTRAIT_WIDTH, 0, ROBO_PORTRAIT_WIDTH, ROBO_PORTRAIT_HEIGHT,
+            handle,
+            null_mut(),
+            null_mut(),
+            null_mut() )
+        };
     }
+
+    // Place the buttons for each item
+    for i in 0..3 {
+        let _ : HWND = unsafe { CreateWindowExW(
+            0,
+            as_wstr("BUTTON").as_ptr(),
+            as_wstr("").as_ptr(),
+            button_style,
+            8*ROBO_PORTRAIT_WIDTH, i*ITEM_PORTRAIT_HEIGHT, ITEM_PORTRAIT_WIDTH, ITEM_PORTRAIT_HEIGHT,
+            handle,
+            null_mut(),
+            null_mut(),
+            null_mut() )
+        };
+    }
+    Ok( Window { handle } )
+
 }
 
 fn handle_message(window: &mut Window) -> bool {
